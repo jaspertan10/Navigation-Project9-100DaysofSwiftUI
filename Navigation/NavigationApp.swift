3d23a08063fe25dab52c026ff6d8bf7c86883af8
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-02 18:18:16 -0800

    iniital commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Jasper Tan on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
